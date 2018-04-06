e5b927d816c44981df18e6653fcd523d972b3b4d
quique1234 <quique00@hotmail.es>
2018-04-06 13:48:10 -0500

    TERMINADO

---
+++ DataSet.py
@@ -0,0 +1,17 @@
+#LUIS ENRIQUE GUZMAN NIÑO
+import numpy as np
+import urllib
+# URL for the Pima Indians Diabetes dataset (UCI Machine Learning Repository)
+url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/pima-indians-diabetes.data.csv"
+# download the file
+raw_data = urllib.urlopen(url)
+# load the CSV file as a numpy matrix
+dataset = np.loadtxt(raw_data, delimiter=",")
+print(dataset.shape)
+# separate the data from the target attributes
+X = dataset[:,0:7]
+y = dataset[:,8]
+
+
+iris = load_iris()
+print(iris)
